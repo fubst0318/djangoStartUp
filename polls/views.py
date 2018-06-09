dd1c291d054c1b427f7abedd61e07c56b40431cd
fubst0318 <fubst0318@126.com>
2018-06-09 19:30:49 +0800

    update a bug

--- polls/views.py
+++ polls/views.py
@@ -19,7 +19,7 @@ class DetailView(generic.DetailView):
     template_name = 'polls/detail.html'
 
 
-class ResultsView(generic.DeleteView):
+class ResultsView(generic.DetailView):
     model = Question
     template_name = 'polls/results.html'
 
